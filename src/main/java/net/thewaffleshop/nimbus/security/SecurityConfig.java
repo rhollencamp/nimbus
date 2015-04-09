921245ff21e2430cdda8354e0ba877d2eeae3850
Robert Hollencamp <rhollencamp@gmail.com>
2015-04-09 16:55:02 -0400

    add apache v2 licensing

--- src/main/java/net/thewaffleshop/nimbus/security/SecurityConfig.java
+++ src/main/java/net/thewaffleshop/nimbus/security/SecurityConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.thewaffleshop.nimbus.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +27,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 /**
+ * Spring Security configuration
  *
  * @author rhollencamp
  */
@@ -29,6 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception
 	{
 		auth
+				/* we do not want to erase credentials after authentication; we need the credentials to be available
+				 * to the app in the auth callbacks */
 				.eraseCredentials(false)
 				.userDetailsService(userDetailsService)
 				.passwordEncoder(passwordEncoder);
